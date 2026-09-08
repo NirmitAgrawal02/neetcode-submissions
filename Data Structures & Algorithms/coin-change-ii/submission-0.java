41905e310b299f7fe425bd92ef9d0b3d78064602
Nirmit Agrawal <38294311+NirmitAgrawal02@users.noreply.github.com>
2026-09-08 18:18:45 +0530

    Update: coin-change-ii - submission-0

--- Data Structures & Algorithms/coin-change-ii/submission-0.java
+++ Data Structures & Algorithms/coin-change-ii/submission-0.java
@@ -1,14 +1,22 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[] count = new int[amount + 1];
-        count[0] = 1;
+        int[][] total = new int[coins.length + 1][amount + 1];
+        Arrays.sort(coins);
+        for(int i = 0; i <= coins.length; i++)
+        {
+            total[i][0] = 1;
+        }
         for(int i = coins.length - 1; i >= 0; i--)
         {
-            for(int a = 1; a <= amount; a++)
+            for(int j = 0; j <= amount;j++)
             {
-                count[a] = count[a] + (coins[i] <= a ? count[a - coins[i]] : 0);
+                if(j >= coins[i])
+                {
+                    total[i][j] = total[i + 1][j];
+                    total[i][j] += total[i][j - coins[i]]; 
+                }
             }
         }
-        return count[amount];
+        return total[0][amount];
     }
 }
